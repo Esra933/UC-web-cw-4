8217aca84231655a1624cfdf5cf0bece6f8ffacb
Esra alsaleh <107806762+Esra933@users.noreply.github.com>
2022-07-02 21:37:28 +0300

    done

---
+++ javascript.js
@@ -0,0 +1,28 @@
+alert('مرحباً بك في موقعي! أنا اسراء وسوف أحسب درجتك')
+let grade= prompt('قم بادخال درجتك');
+alert(grade);
+console.log('%cgrade','color:#bada55');
+
+
+if ( grade <= 100 && grade >= 90 ) {
+    alert("لقد حصلت على امتياز")} 
+    else if ( grade <= 89 && grade >= 80 ) {
+        alert("لقد حصلت على جيد جداً");
+    } else if ( grade <= 79 && grade >= 70 ) { 
+        alert("لقد حصلت على جيد");
+    } else if ( grade <= 69 && grade >= 60 ) {
+        alert("لقد حصلت على مقبول");
+    } else if ( grade <= 59 && grade >= 50 ) {
+        alert(" لقد حصلت على ضعيف");
+    } else{
+        alert("راسب");}
+
+//جافاسكربت ليست جاقا//
+
+
+
+    
+
+
+
+ 
